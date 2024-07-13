e439dfd1e92f64d02092f858a65a308d8e1382c6
Venugopal <karthivenu49@gmail.com>
2024-07-14 00:26:07 +0530

    Fourth commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from 'react-scroll';
 const Home = () => {
   return (
-    <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-20  '>
+    <div name="home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-30  '>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
         <div className='flex flex-col justify-center h-full'>
             <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
@@ -16,7 +16,7 @@ const Home = () => {
             </div>
         </div>
         <div>
-            <img src={mypic} alt='My Profile' className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+            <img src={mypic} alt='My Profile' className='rounded-2xl mx-auto w-2/3 md:w-full pt-10'/>
         </div>
       </div>
         
