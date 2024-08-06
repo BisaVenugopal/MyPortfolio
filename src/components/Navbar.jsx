e461e8878f0c881e7016558fb00f7a1a1c76f5a6
B VENUGOPAL <133234255+BisaVenugopal@users.noreply.github.com>
2024-08-06 19:36:10 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,14 +29,14 @@ const Navbar = () => {
     ];
 
     return (
-        <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-50'>
+        <div className='fixed top-0 w-full h-20 bg-black text-white z-50 flex justify-between items-center px-4'>
             <div>
                 <h1 className='text-5xl font-signature ml-2'>Venugopal</h1>
             </div>
             <ul className='hidden md:flex'>
                 {links.map(({ id, link }) => (
                     <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
-                        <Link to={link} smooth duration={500}>
+                        <Link to={link} smooth duration={500} offset={-80}>
                             {link}
                         </Link>
                     </li>
@@ -49,7 +49,7 @@ const Navbar = () => {
                 <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
                     {links.map(({ id, link }) => (
                         <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
-                            <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>
+                            <Link onClick={() => setNav(!nav)} to={link} smooth duration={500} offset={-80}>
                                 {link}
                             </Link>
                         </li>
